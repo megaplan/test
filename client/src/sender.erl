46df5b13009fdebc2efafc0cbc431059618e5d4c
Arkady Drovosekov <arkdro@gmail.com>
2011-09-12 12:52:18 +0400

    use port from config

--- client/src/sender.erl
+++ client/src/sender.erl
@@ -16,6 +16,7 @@
 
 start(Rses) ->
 	Host = Rses#rses.host,
+	Port = Rses#rses.port,
 	User = Rses#rses.user,
 	Password = Rses#rses.password,
 	Vhost = Rses#rses.vhost,
@@ -23,6 +24,7 @@ start(Rses) ->
 		username = User,
 		password = Password,
 		host = Host,
+        port = Port,
 		virtual_host = Vhost
 		}),
 	{ok, Channel} = amqp_connection:open_channel(Connection),
